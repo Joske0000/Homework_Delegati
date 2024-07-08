ca728c11c3496931b0805e7111e0f999a73290ae
Josip Borozan <joske0000@gmail.com>
2024-07-08 21:17:20 +0200

    add cijenik to git

---
+++ Cijenik.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework_Delegati 
+{
+    
+    internal class Cijena 
+    {
+       public event EventHandler<double> CijenaPromjenjena;
+
+        private static double _vrijednostMPC;
+        private double VrijednostMPC
+        {
+            get { return _vrijednostMPC; }
+            set {
+                _vrijednostMPC = value;
+                CijenaPromjenjena?.Invoke(this, value);
+            }
+        }
+        private static double _vrijednostVPC;
+        public double VrijednostVPC {
+               get { return _vrijednostVPC; }
+               set {
+                   _vrijednostVPC = value;
+                   CijenaPromjenjena?.Invoke(this, value);
+               } 
+        } 
+        
+        public void PovecajMaloprodajnuCijenu (double novaCijena)
+        {
+            VrijednostMPC += novaCijena;
+        }
+        public void PovecajVeleprodajnuCijenu (double novaCijena)
+        {
+            VrijednostVPC += novaCijena;
+        }
+    }
+}
