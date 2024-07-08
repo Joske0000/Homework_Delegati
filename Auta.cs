9ee3fc59ff79b60638c7023564f30313094cfd7a
Josip Borozan <joske0000@gmail.com>
2024-07-08 12:55:26 +0200

    d

--- Auta.cs
+++ Auta.cs
@@ -8,5 +8,21 @@ namespace Homework_Delegati
 {
     internal class Auta
     {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int GodinaProizvodnje { get; set; }
+
+        public int Trzisna_Cijena { get; set; }
+        public Auta()
+        {
+            Trzisna_Cijena = new Cijena();
+           
+
+        }
+
+        private void Promijena_cijene(object sender, int nova_cijena)
+        {
+            Console.WriteLine($"Cijena je promijenjena na {nova_cijena}");
+        }
     }
 }
